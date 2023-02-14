08c4727f57f2cfa58803e8227e457537c5145285
Felipe Vogt <fvogt@bcnschool.cl>
2023-02-14 16:00:37 -0300

    first commit

---
+++ src/components/SobreMi.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <div class="sobre-mi">
+    <div class="titulo text-h2 fade-in">Sobre mi</div>
+
+    <div class="contenido text-p fade-in">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.sobre-mi {
+  margin-top: 100px;
+  .contenido {
+    animation-delay: 200ms;
+    margin-top: 20px;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,77 @@
+<script lang="ts">
+import Presentacion from "../components/Presentacion.vue";
+import SobreMi from "./../components/SobreMi.vue";
+import Experiencia from "./../components/Experiencia.vue";
+
+export default {
+  components: {
+    Presentacion,
+    SobreMi,
+    Experiencia, 
+  },
+  mounted() {
+    const observer = new IntersectionObserver((entries) => {
+      // Loop over the entries
+      entries.forEach((entry) => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+          // Add the animation class
+          entry.target.classList.add("fade-in-animation");
+          return;
+        }
+      });
+    });
+
+    this.$el.querySelectorAll(".fade-in").forEach((i: any) => {
+      if (i) {
+        observer.observe(i);
+      }
+    });
+  },
+};
+</script>
+
+<template>
+  <main>
+    <section class="main-background">
+      <Presentacion />
+    </section>
+    <section>
+      <SobreMi />
+    </section>
+    <section>
+      <Experiencia />
+    </section>
+    <section>
+      <Experiencia />
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+.main-background {
+  background-color: var(--base-color);
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+section > div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px 10px;
+  width: 90%;
+}
+
+@media screen and (min-width: 992px) {
+  section > div {
+    width: 50%;
+    padding-left: 150px;
+  }
+}
+</style>
